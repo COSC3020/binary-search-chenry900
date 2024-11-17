bea96ee76794204fa31d13b7a4e0aa6cca169046
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-17 10:24:51 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ function binarySearch(list, element) {
     for (i = 0; i < list.length; i++)
         {
             if (list[i] == element)
-                return 
+                return i;
+        }
     return -1;
 }
